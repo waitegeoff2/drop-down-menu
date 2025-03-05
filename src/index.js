b525ee6be3729f0b6f3b2e5fdc43d89452830cf2
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-03-05 10:14:01 -0500

    Added logic to bring bar down and change on hover

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import "./styles.css"; 
+
+const dropdownMenu = document.querySelector(".dropdown-menu");
+const dropdownButton = document.querySelector(".dropdown-root");
+const dropdownList = document.querySelector(".dropdown-list");
+const dropdownOption = document.querySelectorAll(".dd-option");
+
+dropdownButton.addEventListener("click", () =>{
+    if(dropdownList.classList.contains("dropdown-list")) {
+        dropdownList.classList.add("dropdown-visible");
+        dropdownList.classList.remove("dropdown-list");
+    } else {
+        dropdownList.classList.remove("dropdown-visible");
+        dropdownList.classList.add("dropdown-list");
+    }
+})
+
+dropdownOption.forEach((button) => {
+    button.addEventListener('mouseenter', () => {
+        button.classList.add('dropdown-hover');
+    })
+})
+
+dropdownOption.forEach((button) => {
+    button.addEventListener('mouseleave', () => {
+        button.classList.remove('dropdown-hover');
+    })
+})
+
+dropdownOption.forEach((button) => {
+    button.addEventListener('click', () => {
+        //CLICK BEHAVIOR
+    })
+})
+
